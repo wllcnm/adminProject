aa23f71fd4a40862e6334ac60284dddf69890055
wllcnm <1712642521@qq.com>
2025-06-18 00:19:43 +0800

    test

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ export const layout = () => {
   return {
     logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
     menu: {
-      locale: false,
+      locale: true,
     },
   };
 };
